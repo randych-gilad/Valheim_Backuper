6ce4072a6ec7ab31ed5f3e2d2819cd4b376fd774
Randych <59070097+randych-gilad@users.noreply.github.com>
2021-03-11 00:10:54 +0200

    Minor variable update
    $CurrentLog = "$Docs\ValheimBackupCurrent.log"
    Just to reduce amount repeating text.

--- ValheimBackup.ps1
+++ ValheimBackup.ps1
@@ -2,7 +2,7 @@
 # Output of Folder creation cmdlet is nulled for reasons
 # Logging is divided by launches and appends
 
-# Define documents Folder cuz I'm lazy
+# Define documents folder cuz I'm lazy
 $Docs = "$env:USERPROFILE\Documents"
 
 # Define screenshot folder, to purge
@@ -11,13 +11,16 @@ $Folder = "$env:USERPROFILE\AppData\LocalLow\"
 # Defines base directory path
 $BackupDir = "$Docs\ValheimBackups\"
 
-Write-Output "Backup creation: $(Get-Date -Format "yyyy:MM:dd HH:mm:ss")" >> "$Docs\ValheimBackupCurrent.log"
+# Defines current log file
+$CurrentLog = "$Docs\ValheimBackupCurrent.log"
+
+Write-Output "Backup creation: $(Get-Date -Format "yyyy.MM.dd HH:mm:ss")" >> $CurrentLog
 
 # Create directory for backups if it doesn't exist
 if (-not (Test-Path -Path $BackupDir)) {
-    Write-Output "`nDirectory $BackupDir doesn't exist." >> $Docs\ValheimBackupCurrent.log
+    Write-Output "`nDirectory $BackupDir doesn't exist." >> $CurrentLog
     New-Item -ItemType Directory -Path "$BackupDir" | Out-Null
-    Write-Output "`nCreated $BackupDir." >> $Docs\ValheimBackupCurrent.log
+    Write-Output "`nCreated $BackupDir." >> $CurrentLog
     }
 
 # Now make it have a cozy gear icon
@@ -39,14 +42,14 @@ Add-Content "$($BackupDir)\desktop.ini" -Value $DesktopIni
 # Purge screenshots
 if (Test-Path -Path "$Folder\IronGate\Valheim\screenshots\") {
     Remove-Item $Folder\IronGate\Valheim\screenshots\
-    Write-Output "`nPurged screenshot folder before creating backup."  >> $Docs\ValheimBackupCurrent.log
+    Write-Output "`nPurged screenshot folder before creating backup."  >> $CurrentLog
     }
 
 # Compress worlds and characters
 &Compress-Archive `
 -Path $Folder\IronGate `
 -DestinationPath "$BackupDir\$(Get-Date -Format "yyyy_MM_dd-HH_mm_ss")_Backup"
-Write-Output "`nCreated $(Get-Date -Format "yyyy_MM_dd-HH_mm_ss")_Backup`n" >> $Docs\ValheimBackupCurrent.log
+Write-Output "`nCreated $(Get-Date -Format "yyyy_MM_dd-HH_mm_ss")_Backup`n" >> $CurrentLog
 
 # Defines how many files you want to keep
 $Keep = 3
@@ -68,7 +71,7 @@ If ($AllFiles.count -gt $Keep) {
 
     # Write about deletion
 
-    Write-Output "There are more than $Keep backups. `nDeleting oldest backups." >> $Docs\ValheimBackupCurrent.log
+    Write-Output "There are more than $Keep backups. `nDeleting oldest backups." >> $CurrentLog
 
     # ForEach loop that goes through the DeleteFile array
     ForEach ($DeleteFile in $DeleteFiles) {
@@ -80,23 +83,23 @@ If ($AllFiles.count -gt $Keep) {
         Remove-Item $dFile
 
         #Informs which files were deleted
-		Write-Output "`nDeleted $($DeleteFile.Basename)" >> $Docs\ValheimBackupCurrent.log
+		Write-Output "`nDeleted $($DeleteFile.Basename)" >> $CurrentLog
     }
 }
 
 # Mark an end of log entry
-Write-Output $("-" * 36) >> "$Docs\ValheimBackupCurrent.log"
+Write-Output $("-" * 36) >> $CurrentLog
 
 # In case someone is running this from console and is lazy to open file
-Get-Content $Docs\ValheimBackupCurrent.log
+Get-Content $CurrentLog
 
 # Check if log file was created
-# Append current log to ongoing log
+# Prepend current log to ongoing log
 
 if (-not (Test-Path -Path $Docs\ValheimBackup.log)) {
     New-Item $Docs\ValheimBackup.log | Out-Null
     }
 
 #Workaround to actually PREPEND
-@($(Get-Content $Docs\ValheimBackupCurrent.log) + $(Get-Content $Docs\ValheimBackup.log)) | Set-Content $Docs\ValheimBackup.log | Out-Null
-Remove-Item $Docs\ValheimBackupCurrent.log
+@($(Get-Content $CurrentLog) + $(Get-Content $Docs\ValheimBackup.log)) | Set-Content $Docs\ValheimBackup.log | Out-Null
+Remove-Item $CurrentLog
